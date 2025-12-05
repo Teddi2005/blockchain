fb87aaf7b2cdf19070f0c67eb9ca1f3c3e1a269b
tiendungql <tiendungql1505@gmail.com>
2025-12-05 21:34:35 +0700

    Update utils.js

--- front/js/utils.js
+++ front/js/utils.js
@@ -1,11 +1,15 @@
 // js/utils.js
+
+//định dạng số thành tiền tệ Việt Nam
 const formatCurrency = (val) =>
   new Intl.NumberFormat("vi-VN", {
     style: "currency",
     currency: "VND",
   }).format(val);
 
+//message
 function showToast(title, msg, type = "info") {
+  //lấy các phần tử HTML của Toast
   const toast = document.getElementById("toast");
   const icon = document.getElementById("toast-icon");
   const titleEl = document.getElementById("toast-title");
@@ -13,10 +17,11 @@ function showToast(title, msg, type = "info") {
 
   if (!toast) return;
 
+  //cập nhật nội dung văn bản
   titleEl.innerText = title;
   msgEl.innerText = msg;
 
-  // Reset classes
+  // Xử lý màu sắc và icon
   toast.classList.remove(
     "border-blue-500",
     "border-green-500",
@@ -28,15 +33,18 @@ function showToast(title, msg, type = "info") {
     icon.innerHTML =
       '<i class="fa-solid fa-circle-check text-green-500 text-xl"></i>';
   } else if (type === "error") {
+    // Màu đỏ + Icon chấm than
     toast.classList.add("border-red-500");
     icon.innerHTML =
       '<i class="fa-solid fa-circle-exclamation text-red-500 text-xl"></i>';
   } else {
+    // Màu xanh dương (mặc định) + Icon chữ i
     toast.classList.add("border-blue-500");
     icon.innerHTML =
       '<i class="fa-solid fa-circle-info text-blue-500 text-xl"></i>';
   }
 
+  // Xóa class 'translate-x-full' để Toast trượt từ phải vào màn hình
   toast.classList.remove("translate-x-full");
   setTimeout(() => toast.classList.add("translate-x-full"), 3000);
 }
